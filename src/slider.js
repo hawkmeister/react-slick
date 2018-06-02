c70153ba23a3b531da7d2af341e0a6dbb8c4a159
Kim Jami <kim@atomicpixel.se>
2018-06-02 11:57:35 +0200

    Revert "Removed dist folder from .gitignore"
    This reverts commit edbc8df0c8c10b5e8b7ce55cfb8c0ffeeeaf6fdc.

--- src/slider.js
+++ src/slider.js
@@ -26,7 +26,6 @@ export default class Slider extends React.Component {
 
   // handles responsive breakpoints
   componentWillMount() {
-    console.log("MOTHER FUCKER");
     // performance monitoring
     //if (process.env.NODE_ENV !== 'production') {
     //const { whyDidYouUpdate } = require('why-did-you-update')
